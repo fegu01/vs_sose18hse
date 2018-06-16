523f48a161223f6c515d196524ef261743fcfe7c
fegu01 <feguit00@hs-esslingen.de>
2018-06-16 21:43:36 +0200

    Init commit

---
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -0,0 +1,88 @@
+package com.example.easynotes.controller;
+
+import com.example.easynotes.exception.ResourceNotFoundException;
+import com.example.easynotes.model.Note;
+import com.example.easynotes.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NoteController {
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+    
+    @PostMapping("/notes2")
+    public void createNote2(@RequestBody Note input) {
+      
+    System.out.println(input);
+    }
+
+    @GetMapping("/notes/{id}")
+    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
+        return noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    }
+
+    @PutMapping("/notes/{id}")
+    public Note updateNote(@PathVariable(value = "id") Long noteId,
+                                           @Valid @RequestBody Note noteDetails) {
+
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+
+        Note updatedNote = noteRepository.save(note);
+        return updatedNote;
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+
+        noteRepository.delete(note);
+
+        return ResponseEntity.ok().build();
+    }
+    
+    
+    
+    
+    
+    
+    
+    @RequestMapping(value = "/addnegative", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void updateminus(@RequestBody Note input) {
+    	System.out.println(input.getContent());
+    	System.out.println(input);		
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ target/classes/static/hello.js
@@ -0,0 +1,20 @@
+class Person {
+	   constructor(id,title,content,createdAt,updatedAt){
+		   this.id=id;
+		   this.title=title;
+		   this.content=content;
+		   this.createdAt=createdAt;
+		   this.updatedat=updatedAt;
+	   }
+	}
+
+
+
+
+angular.module('spring-boot-mysql-rest-api-tutorial', [])
+.controller('Hello', function($scope, $http) {
+    $http.get('http://localhost:8080/api/notes').
+        then(function(response) {
+            $scope.greeting = angular.fromJson(response.data);
+        });
+});
